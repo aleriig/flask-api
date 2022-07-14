9458198ffdfeedbea6c4b8c96a4cdbdf34fac42d
Alejo Rios <alejoriosiglesias@gmail.com>
2022-07-14 20:48:25 +0100

    feat: 1er endpoint, lista de movimientos

--- balance/views.py
+++ balance/views.py
@@ -1,7 +1,29 @@
-from . import app
+from flask import jsonify
 
-@app.route('/')
-def inicio():
-    # por si una linea es muy larga se puede hacer de esta manera
-    return (f"La ruta del archivo de datos es:"
-            f"{app.config['RUTA']}")
+from . import app
+from .models import DBManager
+
+
+"""
+Verbos y formato de endpoints
+    
+GET /movimientos -----> LISTAR movimientos
+POST /movimientos ----> CREAR movimiento
+GET /movimientos/1 ---> LEER movimiento con ID 1
+POST /movimiento/1 ---> ACTUALIZAR el movimiento con ID 1 (sobreescribe todo el objeto)
+PUT /movimientos/1 ---> ACTUALIZAR el movimiento con ID 1 (sobreescribe parcialmente)
+DELETE /movimientos/1 -> ELIMINAR el movimiento con ID 1
+
+IMPORTANTE versionar los endpoints (son un contrato)
+/api/v1/.....
+"""
+
+RUTA = app.config.get("RUTA")
+
+@app.route('/api/v1/movimientos')
+def listar_movimientos():
+    db = DBManager(RUTA)
+    sql = "SELECT * movimientos ORDER BY fecha, id"
+    movmientos = db.consulta_SQL(sql)
+    return jsonify(movmientos)
+    
